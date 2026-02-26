5caecf87fa3a9e57b0b3619a473b0431866f8b67
m1lker-go <m1lker994@gmail.com>
2026-02-26 20:08:22 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use('/shop', require('./routes/shop'));
 app.use('/market', require('./routes/market'));
 app.use('/battle', require('./routes/battle'));
 app.use('/tasks', require('./routes/tasks'));
+app.use('/avatars', require('./routes/avatars'));
 
 const PORT = process.env.PORT || 3000;
 

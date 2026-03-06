a98358c7ba1c0c6e4d141a8585d56db356ce4991
m1lker-go <m1lker994@gmail.com>
2026-03-06 22:59:32 +0300

    Update constants.js

--- client/js/constants.js
+++ client/js/constants.js
@@ -208,6 +208,7 @@ const skinNameTranslations = {
     'skin7': 'Чародей',
     'skin9': 'Магический снежок',
     'skin10': 'Страж королевства'
+     'firelegend': 'Разрушитель'
 };
 
 function translateSkinName(englishName) {

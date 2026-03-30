4622bc81fa691f8eb5691efbf6563e8a5ea97c90
m1lker-go <m1lker994@gmail.com>
2026-03-30 10:38:43 +0300

    Update battleUI.js

--- client/js/battleUI.js
+++ client/js/battleUI.js
@@ -321,24 +321,24 @@ async function showBattleResult(battleData, timeOut = false) {
     });
 
     tabStats.addEventListener('click', () => {
-        tabLog.classList.remove('active');
-        tabStats.classList.add('active');
-        resultDiv.innerHTML = `
-            <table class="stats-table stats-battle">
-                <thead>
-                    <th>Игрок</th><th>Параметр</th><th>Соперник</th>
-                </thead>
-                <tbody>
-                     <tr><td class="player-col">${playerStats.hits} None<td class="player-col">${playerStats.hits}</td><td class="enemy-col">${enemyStats.hits}</td>
-                      <td class="player-col">${playerStats.crits}</td><td>Критов</td><td class="enemy-col">${enemyStats.crits}</td>
-                      <td class="player-col">${playerStats.dodges}</td><td>Уклонений</td><td class="enemy-col">${enemyStats.dodges}</td>
-                      <td class="player-col">${playerStats.totalDamage}</td><td>Урона</td><td class="enemy-col">${enemyStats.totalDamage}</td>
-                      <td class="player-col">${playerStats.heal}</td><td>Исцелено</td><td class="enemy-col">${enemyStats.heal}</td>
-                      <td class="player-col">${playerStats.reflect}</td><td>Отражено</td><td class="enemy-col">${enemyStats.reflect}</td>
-                </tbody>
-             </table>
-        `;
-    });
+    tabLog.classList.remove('active');
+    tabStats.classList.add('active');
+    resultDiv.innerHTML = `
+        <table class="stats-table stats-battle">
+            <thead>
+                <th>Игрок</th><th>Параметр</th><th>Соперник</th>
+            </thead>
+            <tbody>
+                <tr><td class="player-col">${playerStats.hits}</td><td>Ударов</td><td class="enemy-col">${enemyStats.hits}</td></tr>
+                <tr><td class="player-col">${playerStats.crits}</td><td>Критов</td><td class="enemy-col">${enemyStats.crits}</td></tr>
+                <tr><td class="player-col">${playerStats.dodges}</td><td>Уклонений</td><td class="enemy-col">${enemyStats.dodges}</td></tr>
+                <tr><td class="player-col">${playerStats.totalDamage}</td><td>Урона</td><td class="enemy-col">${enemyStats.totalDamage}</td></tr>
+                <tr><td class="player-col">${playerStats.heal}</td><td>Исцелено</td><td class="enemy-col">${enemyStats.heal}</td></tr>
+                <tr><td class="player-col">${playerStats.reflect}</td><td>Отражено</td><td class="enemy-col">${enemyStats.reflect}</td></tr>
+            </tbody>
+        </table>
+    `;
+});
 
     document.getElementById('rematchBtn').addEventListener('click', async () => {
         if (window.battleTimer) clearInterval(window.battleTimer);

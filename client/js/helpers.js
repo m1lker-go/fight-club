5f319f4140849b635738b4d55d81bfb5e502e2eb
m1lker-go <m1lker994@gmail.com>
2026-04-02 00:46:57 +0300

    Update helpers.js

--- client/js/helpers.js
+++ client/js/helpers.js
@@ -437,8 +437,8 @@ function updateMainMenuNewIcons() {
         icon.src = '/assets/icons/icon-new.png';
         icon.className = 'new-icon';
         icon.style.position = 'absolute';
-        icon.style.top = '0px';
-        icon.style.right = '0px';
+        icon.style.top = '-20px';
+        icon.style.right = '-20px';
         icon.style.width = '16px';
         icon.style.height = '16px';
         tasksMenuItem.style.position = 'relative';

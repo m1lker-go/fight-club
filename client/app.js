116ecdfcace47396f5e6fbc13cfb1652f7da4d18
m1lker-go <m1lker994@gmail.com>
2026-04-04 21:17:09 +0300

    Update app.js

--- client/app.js
+++ client/app.js
@@ -37,7 +37,7 @@ let ratingTab = 'rating';
 // Глобальный базовый URL для API
 window.API_BASE = 'https://fight-club-api-4och.onrender.com';
 window.BOT_USERNAME = 'CatFightingBot';
-window.GOOGLE_CLIENT_ID = '777033220750-o667o0cfaa2tb9qnnaj95pph70mv20ob.apps.googleusercontent.com';
+window.GOOGLE_CLIENT_ID = '777033220750-06670cfa2tb9qnnaj95pph70mv20ob.apps.googleusercontent.com';
 
 // ========== ВСПОМОГАТЕЛЬНАЯ ФУНКЦИЯ ДЛЯ ПОВТОРНЫХ ЗАПРОСОВ ==========
 async function fetchWithRetry(url, options, retries = 3, timeout = 40000) {

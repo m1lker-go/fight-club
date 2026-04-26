3f8b4ca71b4eecf134fe817f98180c725b4ca70d
m1lker-go <m1lker994@gmail.com>
2026-04-26 14:37:24 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,9 +12,17 @@ console.log('BOT_USERNAME:', process.env.BOT_USERNAME);
 
 const app = express();
 
-// Временное упрощение CORS – разрешить все источники
+const allowedOrigins = ['https://cat-fight.ru', 'https://www.cat-fight.ru'];
 app.use(cors({
-    origin: '*',
+    origin: function (origin, callback) {
+        // Запросы без origin (мобильные приложения, curl) разрешены
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 app.use(express.json());

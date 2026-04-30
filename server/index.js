ed7465db9977f62bb1783db8e02f0ebd184f3821
m1lker-go <m1lker994@gmail.com>
2026-04-30 19:44:38 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use('/avatars', require('./routes/avatars'));
 app.use('/forge', require('./routes/forge-server'));
 app.use('/tower', require('./routes/tower-server'));
 app.use('/rank', require('./routes/rank'));
-app.use('/fortune', require('./routes/fortune'));
+app.use('/fortune', require('./routes/fortune-server'));
 
 app.post('/auth/vk/callback', (req, res) => {
     console.log('Received VK callback (unexpected, low-code uses callback mode)');

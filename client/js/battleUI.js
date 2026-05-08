a6f498a9d64760decc8e1b76a0e1e88321288a5b
m1lker-go <m1lker994@gmail.com>
2026-05-09 02:16:59 +0300

    Update battleUI.js

--- client/js/battleUI.js
+++ client/js/battleUI.js
@@ -95,7 +95,7 @@ function showBattleScreen(battleData) {
                 <div class="hero-card">
                    <div style="position: relative; width: 110px; height: 165px; margin: 0 auto;">
     <img src="/assets/${userData.avatar || 'cat_heroweb.png'}" alt="hero" class="hero-avatar-img">
-  ${userData.subscription_expiry && new Date(userData.subscription_expiry) > new Date() ? '<i class="fas fa-crown" style="position: absolute; top: 10px; left: 10px; color: #c0c0c0; font-size: 14px; filter: drop-shadow(0 0 2px rgba(0,0,0,0.5)); pointer-events: none; z-index: 5;"></i>' : ''}
+  ${userData.subscription_expiry && new Date(userData.subscription_expiry) > new Date() ? '<i class="fas fa-crown" style="position: absolute; top: 20px; left: 20px; color: #c0c0c0; font-size: 8px; filter: drop-shadow(0 0 2px rgba(0,0,0,0.5)); pointer-events: none; z-index: 5;"></i>' : ''}
     <div class="frozen-overlay"><img src="/assets/fight/frozenx.gif" alt="frozen"></div>
     <div class="defeat-overlay">Проиграл</div>
     <div id="hero-animation" class="animation-container"></div>

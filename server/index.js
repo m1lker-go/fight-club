e595d796158ab404966f04217c7dc643230557ca
m1lker-go <m1lker994@gmail.com>
2026-05-20 23:10:26 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,26 +13,6 @@ console.log('BOT_USERNAME:', process.env.BOT_USERNAME);
 
 const app = express();
 
-// ---------- CORS (исправлено) ----------
-const allowedOrigins = ['https://cat-fight.ru', 'https://www.cat-fight.ru'];
-
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-        // Обработка предварительных запросов (OPTIONS)
-        if (req.method === 'OPTIONS') {
-            return res.sendStatus(200);
-        }
-    }
-    next();
-});
-// ----------------------------------------
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('client'));

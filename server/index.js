9871e261cba6155bb76382c1ddfd4433bcd9bc11
m1lker-go <m1lker994@gmail.com>
2026-05-20 16:13:19 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const cron = require('node-cron');
 const { pool, initDB } = require('./db');
 const { updatePlayerPower } = require('./utils/power');
@@ -14,12 +13,25 @@ console.log('BOT_USERNAME:', process.env.BOT_USERNAME);
 
 const app = express();
 
-app.use(cors({
-    origin: true,                   // временно разрешаем любой origin
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// ---------- CORS (исправлено) ----------
+const allowedOrigins = ['https://cat-fight.ru', 'https://www.cat-fight.ru'];
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        // Обработка предварительных запросов (OPTIONS)
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(200);
+        }
+    }
+    next();
+});
+// ----------------------------------------
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

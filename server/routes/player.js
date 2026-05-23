1f84dbfbd91f8fee5cdebef680efacc755c8c88a
m1lker-go <m1lker994@gmail.com>
2026-05-24 01:22:22 +0300

    Update player.js

--- server/routes/player.js
+++ server/routes/player.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { pool, getUserByIdentifier } = require('../db');
+const { pool } = require('../db');
 const { updatePlayerPower } = require('../utils/power');
 const dailyTasks = require('../utils/dailyTasks');
 

c918dde7096f2a821de41f0c5bab2d38af3371d5
m1lker-go <m1lker994@gmail.com>
2026-06-03 15:07:13 +0300

    Update clans-server.js

--- server/routes/clans-server.js
+++ server/routes/clans-server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { pool } = require('../db');
 
+
 // ------------------- ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ -------------------
 
 // Запрещённые слова (добавьте реальные)
